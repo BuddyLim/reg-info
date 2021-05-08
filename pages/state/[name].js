9bfa8026b8142b5498f5037c9a00d830732f6a30
Buddy Lim <buddy.tlimk@gmail.com>
2021-05-08 23:24:55 +0800

    [Fix] Removed useQuery for state name

--- pages/state/[name].js
+++ pages/state/[name].js
@@ -1,20 +1,8 @@
 import { useRouter } from "next/router";
-import { useQuery, gql } from '@apollo/client';
-
-const TEST_QUERY = gql`
-  query MyQuery {
-    states_state_name {
-      id
-      name
-    }
-  }`
 
 
 export default function StateTemplate() {
   const router = useRouter();
-  const test = useQuery(TEST_QUERY)
-
-  console.log("🚀 ~ file: index.js ~ line 26 ~ Home ~ test", test)
   
   return (
     <div>
